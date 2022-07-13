4b8329f9f9dedac1325f2dd5388b427a3983b619
leephu4293 <lehongphu4293@gmail.com>
2022-07-13 14:30:23 +0700

    fdf

---
+++ 01_db_overview/practice/thuchanh.sql
@@ -0,0 +1,9 @@
+create database my_database;
+use my_database;
+create table student (
+id_student int auto_increment,
+name_student  varchar(40), 
+age int, 
+country varchar(30),
+primary key (id_student)
+);
