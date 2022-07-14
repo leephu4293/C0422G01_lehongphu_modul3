88d2124182b2265a572317ed71555fa19e880bdf
leephu4293 <lehongphu4293@gmail.com>
2022-07-14 22:28:09 +0700

    up

--- case_study_db/casestudy.sql
+++ case_study_db/casestudy.sql
@@ -1,6 +1,7 @@
-create database FuramaResort;
+DROP DATABASE IF EXISTS FuramaResort;
+CREATE DATABASE FuramaResort;
 
-use furamaresort;
+USE furamaresort;
 
 CREATE TABLE vi_tri (
     ma_vi_tri INT PRIMARY KEY,
@@ -26,41 +27,40 @@ CREATE TABLE nhan_vien (
     so_dien_thoai VARCHAR(45),
     email VARCHAR(45),
     dia_chi VARCHAR(45),
-    ma_vi_tri INT,
-    ma_trinh_do INT,
-    ma_bo_phan INT
+    ma_vi_tri INT NOT NULL,
+    ma_trinh_do INT NOT NULL,
+    ma_bo_phan INT NOT NULL,
+    FOREIGN KEY (ma_vi_tri) REFERENCES vi_tri(ma_vi_tri),
+    FOREIGN KEY (ma_trinh_do) REFERENCES trinh_do(ma_trinh_do),
+    FOREIGN KEY (ma_bo_phan) REFERENCES bo_phan (ma_bo_phan)
 ); 
-
-CREATE TABLE hop_dong (
-    ma_hop_dong INT PRIMARY KEY,
-    ngay_lam_hop_dong DATETIME,
-    ngay_ket_thuc DATETIME,
-    tien_dat_coc DOUBLE,
-    ma_nhan_vien INT,
-    ma_khach_hang INT,
-    ma_dich_vu INT
-);
-
-CREATE TABLE khach_hang (
-    ma_khach_hang INT PRIMARY KEY,
-    ma_loai_khach INT,
-    ho_ten VARCHAR(45),
-    ngay_sinh DATE,
-    gioi_tinh BIT(1),
-    so_cmnd VARCHAR(45),
-    so_dien_thoai VARCHAR(45),
-    email VARCHAR(45),
-    dia_chi VARCHAR(45)
-);
 
 CREATE TABLE loai_khach (
     ma_loai_khach INT PRIMARY KEY,
     ten_loai_khach VARCHAR(45)
 );
 
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY,
+    ma_loai_khach INT NOT NULL,
+    ho_ten VARCHAR(45),
+    ngay_sinh DATE,
+    gioi_tinh BIT(1),
+    so_cmnd VARCHAR(45),
+    so_dien_thoai VARCHAR(45),
+    email VARCHAR(45),
+    dia_chi VARCHAR(45),
+	FOREIGN KEY (ma_loai_khach) REFERENCES loai_khach (ma_loai_khach)
+);
+
 CREATE TABLE loai_dich_vu (
     ma_loai_dich_vu INT PRIMARY KEY,
     ten_loai_khach VARCHAR(45)
+);
+
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY,
+    ten_kieu_thue VARCHAR(45)
 );
 
 CREATE TABLE dich_vu (
@@ -69,25 +69,28 @@ CREATE TABLE dich_vu (
     dien_tich INT,
     chi_phi_thue DOUBLE,
     so_nguoi_toi_da INT,
-    ma_kieu_thue INT,
-    ma_loai_dich_vu INT,
+    ma_kieu_thue INT NOT NULL,
+    ma_loai_dich_vu INT NOT NULL,
     tieu_chuan_phong VARCHAR(45),
     mo_ta_tien_nghi_khac VARCHAR(45),
     dien_tich_do_boi DOUBLE,
     so_tang INT,
-    dich_vu_mien_phi_di_kem TEXT
+    dich_vu_mien_phi_di_kem TEXT,
+    FOREIGN KEY (ma_loai_dich_vu) REFERENCES loai_dich_vu (ma_loai_dich_vu),
+    FOREIGN KEY (ma_kieu_thue) REFERENCES kieu_thue (ma_kieu_thue)
 );
 
-CREATE TABLE kieu_thue (
-    ma_kieu_thue INT PRIMARY KEY,
-    ten_kieu_thue VARCHAR(45)
-);
-
-CREATE TABLE hop_dong_chi_tiet (
-    ma_hop_dong_chi_tiet INT,
-    ma_hop_dong INT,
-    ma_dich_vu_di_kem INT,
-    so_luong INT
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY,
+    ngay_lam_hop_dong DATETIME,
+    ngay_ket_thuc DATETIME,
+    tien_dat_coc DOUBLE,
+    ma_nhan_vien INT NOT NULL,
+    ma_khach_hang INT NOT NULL,
+    ma_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_nhan_vien) REFERENCES nhan_vien (ma_nhan_vien),
+    FOREIGN KEY (ma_khach_hang) REFERENCES khach_hang (ma_khach_hang),
+    FOREIGN KEY (ma_dich_vu) REFERENCES dich_vu (ma_dich_vu)
 );
 
 CREATE TABLE dich_vu_di_kem (
@@ -97,6 +100,21 @@ CREATE TABLE dich_vu_di_kem (
     don_vi VARCHAR(10),
     trang_thai VARCHAR(45)
 );
+
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT,
+    ma_hop_dong INT NOT NULL ,
+    ma_dich_vu_di_kem INT NOT NULL,
+    so_luong INT,
+    FOREIGN KEY (ma_hop_dong) REFERENCES hop_dong (ma_hop_dong),
+	FOREIGN KEY  (ma_dich_vu_di_kem) REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem)
+);
+
+
+
+
+
+
 
 
 
