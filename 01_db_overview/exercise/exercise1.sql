9e6238b56fa690f87663a346e79cd2d55575f564
leephu4293 <lehongphu4293@gmail.com>
2022-07-14 09:17:19 +0700

    up

--- 01_db_overview/exercise/exercise1.sql
+++ 01_db_overview/exercise/exercise1.sql
@@ -1,14 +1,22 @@
+drop database if exists my_database; 
+create database my_database;
 use my_database;
+
 CREATE TABLE class (
     id INT,
     name_student VARCHAR(50)
 );
+
 alter table class add primary key (id); 
 create table teacher (
-id int, 
-nameTeacher varchar(50),
-age int, 
+	id int, 
+	nameTeacher varchar(50),
+	age int, 
 country varchar(50)
 );
-insert into class (id,name_student) values (1,"long"),(2,"huy");
-insert into student (name_student,age,country) values ("long",25,"hue"),("huy",24,"aasdd"),("duc",22,"qweqweqe");
+
+insert into class (id,name_student) 
+values (1,"long"),(2,"huy");
+
+insert into student (name_student,age,country) 
+values ("long",25,"hue"),("huy",24,"aasdd"),("duc",22,"qweqweqe");
