f24bde33a03be3b0b527f21e396d601876b24677
leephu4293 <lehongphu4293@gmail.com>
2022-07-17 21:47:44 +0700

    up

--- case_study_db/casestudy.sql
+++ case_study_db/casestudy.sql
@@ -219,6 +219,57 @@ INSERT INTO hop_dong_chi_tiet (ma_hop_dong_chi_tiet, ma_hop_dong, ma_dich_vu_di_
  ('8', '12', '2', '2');
  
  
+ SELECT *
+ FROM nhan_vien n
+ WHERE n.ho_ten like '% H%';
+ 
+-- 3.Hiển thị thông tin của tất cả khách hàng có độ tuổi từ 18 đến 50 tuổi và có địa chỉ ở “Đà Nẵng” hoặc “Quảng Trị”.
+ SELECT * 
+ FROM khach_hang c
+ WHERE  ( c.dia_chi like '% Đà Nẵng' OR c.dia_chi like '% Quảng Trị') AND  ROUND(DATEDIFF(CURDATE(),ngay_sinh)/365) BETWEEN 18 AND 50; 
+ 
+-- Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp 
+-- xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”. 
+SELECT  ho_ten , COUNT(hop_dong.ma_khach_hang) as so_lan_dat_phong 
+FROM khach_hang c 
+JOIN hop_dong 
+ON c.ma_khach_hang = hop_dong.ma_khach_hang
+JOIN loai_khach 
+ON c.ma_loai_khach = loai_khach.ma_loai_khach
+WHERE loai_khach.ma_loai_khach = 1
+GROUP BY c.ho_ten
+ORDER BY so_lan_dat_phong;
+
+SELECT 
+khach_hang.ma_khach_hang, 
+khach_hang.ho_ten, 
+loai_khach.ten_loai_khach, 
+hop_dong.ma_hop_dong, 
+dich_vu.ten_dich_vu, 
+hop_dong.ngay_lam_hop_dong, 
+hop_dong.ngay_ket_thuc, 
+SUM(chi_phi_thue+so_luong*gia)
+FROM khach_hang 
+LEFT JOIN hop_dong
+ON khach_hang.ma_khach_hang= hop_dong.ma_khach_hang
+LEFT JOIN hop_dong_chi_tiet 
+ON hop_dong_chi_tiet.ma_hop_dong = hop_dong.ma_hop_dong
+LEFT JOIN dich_vu_di_kem 
+ON dich_vu_di_kem.ma_dich_vu_di_kem= hop_dong_chi_tiet.ma_hop_dong_chi_tiet
+LEFT JOIN dich_vu 
+ON dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+LEFT JOIN loai_khach 
+ON loai_khach.ma_loai_khach = khach_hang.ma_khach_hang
+GROUP BY hop_dong.ma_hop_dong, khach_hang.ma_khach_hang;
+
+  
+ 
+ 
+ 
+
+ 
+
+ 
 
 
 
